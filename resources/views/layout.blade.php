b622c082b7c2fca996bad2faf0491be2f391d43d
eithorne <eithornee@gmail.com>
2019-10-18 13:12:23 +0200

    Fixed CSS links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>blep | @yield('title')</title>
-        <link href="/css/style.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
     </head>
     <body>   
         @yield('content')
